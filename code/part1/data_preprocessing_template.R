a0a98d014d4633d0b78aa02fcc7ad0e164ba87a1
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-10 13:33:25 +0000

    R fill in missing data

--- code/part1/data_preprocessing_template.R
+++ code/part1/data_preprocessing_template.R
@@ -2,3 +2,16 @@
 
 # Import the dataset
 dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+# ifelse First parameter is the if condition
+# second parameter value input if value is true
+# third parameter value input if value is false
+
+# Checking all values in the column Age
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
