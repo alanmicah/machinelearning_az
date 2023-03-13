7ded50d31eeb2f2304ce8c70923718741586a553
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-13 10:23:37 +0000

    simple linear regression intro

---
+++ code/part2/Simple Regression/simple_linear_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training and the Test data
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+# What: consist of 2 seperate sets, Training set train ml model on existing observations
+# Test set for evaluate of the performance of the model on new observations
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
